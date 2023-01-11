a97e52db86a38b2d5a3f9f65be8d4312910f1e84
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2023-01-11 11:47:34 +0200

    completed task c

--- components/List.js
+++ components/List.js
@@ -1,33 +1,9 @@
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
-import {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(baseUrl + 'media');
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (file) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
-          return await fileResponse.json();
-        })
-      );
-      setMediaArray(media);
-    } catch (error) {
-      console.error('list, loadMedia', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
-  console.log('List, loadName', mediaArray);
-
+  const {mediaArray} = useMedia();
   return (
     <FlatList
       data={mediaArray}
