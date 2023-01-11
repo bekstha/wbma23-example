31e50307667e323f0af813514ba233165b4a9d8d
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2023-01-11 09:08:01 +0200

    first app development

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import List from './components/List';
 
 const App = () => {
   return (
     <>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
