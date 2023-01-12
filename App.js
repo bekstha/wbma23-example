fcbb54137985255bf96b94b1544987768c6de8b2
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2023-01-12 09:58:40 +0200

    navigation

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
-import {StyleSheet, SafeAreaView} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
-import List from './components/List';
+import Navigator from './navigators/Navigator,js';
 
 const App = () => {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+      <Navigator />
       <StatusBar style="auto" />
     </>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
