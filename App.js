9f2373423a6445fe5e00be6506c4cfc433572510
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2023-01-16 14:30:18 +0200

    styling

--- App.js
+++ App.js
@@ -18,8 +18,9 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 30,
   },
 });
