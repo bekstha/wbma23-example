e6695f5c1c4ffb962be203c0b98590710806ce57
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2023-01-16 10:27:13 +0200

    dummy login completed

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
+import {MainProvider} from './contexts/MainContext.js';
 import Navigator from './navigators/Navigator.js';
 
 const App = () => {
   return (
-    <>
+    <MainProvider>
       <Navigator />
       <StatusBar style="auto" />
-    </>
+    </MainProvider>
   );
 };
 
